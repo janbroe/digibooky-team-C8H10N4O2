6d476e8c5c6d75b3967eaf7ccb80a1029376ba59
St12n <stijn.schouppe@hotmail.com>
2022-10-21 16:14:08 +0200

    MemberRepository class is updated.

---
+++ api/src/main/java/com/switchfully/digibooky/api/MemberController.java
@@ -0,0 +1,14 @@
+package com.switchfully.digibooky.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/members")
+public class MemberController {
+
+
+
+
+}
